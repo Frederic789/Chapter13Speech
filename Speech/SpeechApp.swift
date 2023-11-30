60aaba0c32f966c059f4d1277fd8a38c1c35a96c
Freddy <fredericdouamba2014@gmail.com>
2023-11-30 11:33:49 -0800

    Initial Commit

---
+++ Speech/SpeechApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeechApp.swift
+//  Speech
+//
+//  Created by Student Account on 11/30/23.
+//
+
+import SwiftUI
+
+@main
+struct SpeechApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
